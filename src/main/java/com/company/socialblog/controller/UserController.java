71042c257c0fb66117388ec954b763155b12b9d6
Arda Altinay <ardaltinay48@gmail.com>
2020-10-20 14:57:00 +0300

    added home and register page

---
+++ src/main/java/com/company/socialblog/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.company.socialblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+}
