40f22f3e56782115a92e10fe0e50361f6d084049
Arda Altinay <ardaltinay48@gmail.com>
2020-11-27 21:05:46 +0300

    fixed imports

--- src/main/java/com/company/socialblog/controller/SettingsController.java
+++ src/main/java/com/company/socialblog/controller/SettingsController.java
@@ -7,12 +7,13 @@ import com.company.socialblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
